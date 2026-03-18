141e923735e700f29f8e3ec6f89b63845f26da3d
Lucas Orleans <lucasorleans.pro@gmail.com>
2026-03-18 13:42:33 -0300

    Teste React

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Hero from './sections/hero'
+
+function App() {
+  return (
+    <>
+      <Hero /> 
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/sections/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return (
+        <section className="min-h-screen bg-gradient-to-r from-purple-300 via-purple-700 to-blue-700 text-white flex items-center justify-center">
+            Teste
+        </section>
+    )
+}
